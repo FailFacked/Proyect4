b37c3d8c0e3cea8e0605380b0fdaeb9a9e4f9e39
FailFacked <diaz987j@gmail.com>
2025-11-25 22:55:30 -0600

    Actualización completa del proyecto y Docker

---
+++ proyeto4/proyecto4.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace Proyecto4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("bienvenido a proyecto4");
+		}
+	}
+}
